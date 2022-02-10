b80b4bf9b343103a704dc418db16b94a696d23c6
Asha-asr <asharamankutty20@gmail.com>
2022-02-10 09:02:03 +0530

    First Commit

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,79 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            All Projects
+        </h2>
+    </x-slot>
+
+    <div class="py-m-6">
+        <div class="w-full m-2">
+                @can('create client')
+                <a href="{{ route('clients.create')}}" class="m-2 p-2 bg-green-400 rounded">
+                    Create Project
+                </a>
+                @endcan
+                
+                
+            </div>
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+
+        
+
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50 dark:bg-gray-600 dark:text-gray-200">
+                <tr>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Title</th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Assigned to</th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Client</th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Deadline</th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Status</th>
+                    <!-- <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Client</th> -->
+                   
+                </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                <tr></tr>
+
+                @foreach (App\Models\Project::all() as $project )
+
+                <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">{{ $project->title }}</td>
+                    <td class="px-6 py-4 whitespace-nowrap">{{ $project->user_id }}</td>
+                    <td class="px-6 py-4 whitespace-nowrap">{{ $project->client_id }}</td>
+                    <td class="px-6 py-4 whitespace-nowrap">{{ $project->deadline }}</td>
+                    <!-- <td class="px-6 py-4 whitespace-nowrap">{{ $project->status }}</td> -->
+                    <!-- <td class="px-6 py-4 whitespace-nowrap">
+                    <img class="w-8 h-8 rounded-full" src="https://picsum.photos/200" />
+                    </td> -->
+                    <td class="px-6 py-4 text-right text-sm">
+                        @can('edit client')
+                        <a href="" class="m-2 p-2 bg-green-400 rounded">Edit</a>
+                        @endcan
+                        @can('delete client')
+                        <a href="#" class="m-2 p-2 bg-green-400 rounded">Delete</a>
+                        @endcan
+                    </td>
+                </tr>
+
+                @endforeach
+
+                <!-- <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">id</td>
+                            <td class="px-6 py-4 whitespace-nowrap">title</td>
+                            <td class="px-6 py-4 whitespace-nowrap">Active</td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                            <img class="w-8 h-8 rounded-full" src="https://picsum.photos/200" />
+                    </td>
+                    <td class="px-6 py-4 text-right text-sm">Edit Delete</td>
+                </tr> -->
+                <!-- More items... -->
+                </tbody>
+            </table>
+            <div class="m-2 p-2">Pagination</div>
+            </div>
+        </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::get('/projects', function () {
+    return view('projects.index');
+})->middleware(['auth'])->name('projects.index');
+
+Route::get('clients/create', [ClientController::class, 'create'])->name('clients.create')->middleware('role:admin');
+Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit')->middleware('role:admin|user1|user2|user3');
+
